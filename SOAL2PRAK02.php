8d4530a26de1db114492e105f2fe9d97a79885b1
ListaRerung <85219689+ListaRerung@users.noreply.github.com>
2022-03-25 14:15:47 +0800

    Update SOAL2PRAK02.php

--- SOAL2PRAK02.php
+++ SOAL2PRAK02.php
@@ -1,53 +1,54 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <style type="text/css">
+        text{color:red;}</style>
+    </head>
 <body>
 <h1>Input Biodata</h1>
 <form action="" method="post">
-Nama: <input type="text" name="nama"><font color="red">*</font>
-<font color="red">
-<?php
-$nama=$_POST['nama'];
+
+Nama: <input type="text" name="nama"><text>*
+<?php $nama=$_POST['nama'];
 if($nama == NULL ){
     echo "nama tidak boleh kosong\n";
-}
-?></font>
+} ?></text>
 <br>
-Nim: <input type="text" name="nim"><font color="red">*</font>
-<font color="red"><?php
-$nim=$_POST["nim"];
+
+Nim: <input type="text" name="nim"><text>*
+<?php $nim=$_POST["nim"];
 if($nim == NULL ){
-    echo "nim tidak boleh kosong\n";
-}
-?></font>
+echo "nim tidak boleh kosong\n";
+}?></text>
 <br>
-Jenis Kelamin:<font color="red">*</font>
-<?php
-        $jeniskelamin =$_POST['jeniskelamin'];
-        if($jeniskelamin == NULL ){
-        echo "jenis kelamin tidak boleh kosong\n";
-        }
-    ?>
+
+Jenis Kelamin:<text>*
+    <?php $jeniskelamin =$_POST['jeniskelamin'];
+if($jeniskelamin == NULL ){
+echo "jenis kelamin tidak boleh kosong\n";
+}?></text>
 <br>
 <input type="radio" name="jeniskelamin" value="Laki-Laki">Laki-Laki
       <br>
 <input type="radio" name="jeniskelamin" value="Perempuan">Perempuan
    <br>
+
 <input type="submit" name="send" value="submit"><br><br>
-
-    <?php
-    echo "$nama";
-    echo "<br>";
-    echo "$nim";
-    echo "<br>";
-    echo "$jeniskelamin";
-
-?>
 </form>
 </body>
 </html>
 
-
-
-
-
-
+<?php
+        if ($nama == TRUE) {
+            echo "$nama";
+            echo "<br>";
+        }
+        if($nim == TRUE){
+            echo "$nim";
+            echo"<br>";
+        }
+        if($jeniskelamin== TRUE){
+                echo "$jeniskelamin";
+                echo "<br>";
+        }
+    ?>
