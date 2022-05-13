8f105ae38fc7e1d6ccfb4d52ec03be38fe762b48
Artemis <85219689+ListaRerung@users.noreply.github.com>
2022-05-13 09:37:26 +0800

    Update Member.php

--- peminjaman/Member.php
+++ peminjaman/Member.php
@@ -5,16 +5,8 @@ if (isset($_GET['id_member'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Member</title>
-</head>
+<html>
 <body>
-    <a href="FormMember.php"><button>Tambah Data</button></a>
     <table border="1">
         <tr>
             <th>Nama</th>
@@ -26,5 +18,6 @@ if (isset($_GET['id_member'])) {
         </tr>
         <?= tampildata("member") ?>
     </table>
+      <a href="FormMember.php"><button>Tambah Data</button></a>
 </body>
 </html>
