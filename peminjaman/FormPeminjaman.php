061dcbc2a26925f6e9de5e0a976e9e86a9e34c8e
Artemis <85219689+ListaRerung@users.noreply.github.com>
2022-05-13 09:36:07 +0800

    Update FormPeminjaman.php

--- peminjaman/FormPeminjaman.php
+++ peminjaman/FormPeminjaman.php
@@ -1,48 +1,58 @@
 <?php require('./Model.php');
-if (isset($_GET['id_peminjaman'])) {
-    editpeminjaman();
+if (isset($_GET['id_buku'])) {
+    editbuku();
 }
-// print_r($result);
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data | Peminjaman</title>
-</head>
-
+<html>
 <body>
     <form action="" method="post">
         <table>
 
             <tr>
                 <td>
-                    Tanggal Peminjaman
+                    Judul Buku
                 </td>
                 <td>
-                    <input type="date" name="tgl_pinjam" <?php echo (isset($_GET['id_peminjaman'])) ?  "value = " . $result[0]["tgl_pinjam"] . "" : "value = '' "; ?> required> <br>
+                    <input type="text" name="judul" <?php echo (isset($_GET['id_buku'])) ?  "value = " . $result[0]["judul_buku"] . "" : "value = '' "; ?> required> <br>
                 </td>
             </tr>
             <tr>
                 <td>
-                    Tanggal Kembali
+                    Nama Penulis
                 </td>
                 <td>
-                    <input type="date" name="tgl_kembali" <?php echo (isset($_GET['id_peminjaman'])) ?  "value = " . $result[0]["tgl_kembali"] . "" : "value = '' "; ?> required> <br>
+                    <input type="text" name="penulis" <?php echo (isset($_GET['id_buku'])) ?  "value = " . $result[0]["penulis"] . "" : "value = '' "; ?> required> <br>
                 </td>
             </tr>
-            
+            <tr>
+                <td>
+                    Penerbit
+                </td>
+                <td>
+
+                <input type="text" name="penerbit" <?php echo (isset($_GET['id_buku'])) ?  "value = " . $result[0]["penerbit"] . "" : "value = '' "; ?> required> <br>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Tahun Terbit
+
+                </td>
+                <td>
+                <input type="text" name="thnterbit" <?php echo (isset($_GET['id_buku'])) ?  "value = " . $result[0]["tahun_terbit"] . "" : "value = '' "; ?> required> <br>
+
+                </td>
+            </tr>
             <tr>
                 <td></td>
                 <td>
                     <?php 
-                    if (isset($_GET['id_peminjaman'])) {
+                    if (isset($_GET['id_buku'])) {
                         echo "<button type=\"submit\" name=\"update\">Update</button>";
                     }else {
                         echo "<button type=\"submit\" name=\"submit\">Tambah</button>";
+                        
                     }
 
                     ?>
@@ -50,15 +60,13 @@ if (isset($_GET['id_peminjaman'])) {
             </tr>
         </table>
     </form>
+
     <?php
     if (isset($_POST['submit'])) {
-       
-        // print_r($_POST);
-        tambahdatapeminjaman($_POST['tgl_pinjam'], $_POST['tgl_kembali']);
-        // tambahdatapeminjaman('2022-05-05', '2022-05-06');
+        tambahdatabuku($_POST['judul'], $_POST['penulis'], $_POST['penerbit'], $_POST['thnterbit']);
     }
     if (isset($_POST['update'])) {
-        updatepeminjaman($_GET['id_peminjaman'], $_POST['tgl_pinjam'], $_POST['tgl_kembali']);
+       updatebuku($_GET['id_buku'],$_POST['judul'],$_POST['penulis'],$_POST['penerbit'],$_POST['thnterbit']);
     }
     ?>
 </body>
