ec91ca644836d26bc125dc2b7144c5eb6e7f9b3f
Artemis <85219689+ListaRerung@users.noreply.github.com>
2022-05-13 10:01:11 +0800

    Update FormMember.php

--- peminjaman/FormMember.php
+++ peminjaman/FormMember.php
@@ -4,15 +4,7 @@ if (isset($_GET['id_member'])) {
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data | Member</title>
-</head>
+<html>
 
 <body>
     <form action="" method="post">
